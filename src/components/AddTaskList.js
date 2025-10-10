57abe8406e7d3de1a063df2d687c907cbfeb5c31
Arvy <arvymagimot07@gmail.com>
2025-10-10 21:13:27 +0800

    Task2Router&ShellDeveloper

---
+++ src/components/AddTaskList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddTaskView() {
+  return (
+    <div className="container mt-4">
+      <h2>Add Task View</h2>
+      <p>This is where the Add Task form will go.</p>
+    </div>
+  );
+}
+
+export default AddTaskView;
---
+++ src/components/TaskListView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TaskListView() {
+  return (
+    <div className="container mt-4">
+      <h2>Task List View</h2>
+    </div>
+  );
+}
+
+export default TaskListView;
